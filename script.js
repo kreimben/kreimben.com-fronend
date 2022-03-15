a9e0c3286dc55fde3f01df4464adcdd9fde29398
Aksidion Kreimben <aksidion@kreimben.com>
2022-03-15 23:55:13 +0900

    Set to public repository to deploy server directly.

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function init() {
+const set_title = () => {
   document.title = "Kreimben.com";
+}
+
+const set_body = () => {
+  document.body = document.createElement(`body`);
+  document.body.innerHTML = `<p style='background:red'>Hello!</p1>`;
 }
 
 var app = document.getElementsByClassName("App");
 
-async function fetch_data() {
-  alert("in the function \"fetch_data()\"");
-  app.innerHTML = await fetch('https://www.naver.com');
+const fetch_data = async () => {
+
+  const result = fetch('http://localhost:10120/get_photos') // 'https://api.kreimben.com/get_photos')
+        .then(res => res.json());
+
+  app.innerHTML = Stringify(result);
+
+  set_body();
+
+  return result;
 }
 
 
-init();
-fetch_data();
+set_title();
+const json = fetch_data();
+console.log(json == "");
