623fa65d470e7d96f0e439e7a1d7fac98a020ef7
Aksidion Kreimben <aksidion@kreimben.com>
2022-03-18 05:35:34 +0900

    After install svelte template via degit

---
+++ src/components/Home.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+ import { onMount } from '*.svelte';
+
+ let json;
+
+ /*onMount(async () => {
+   const res = await fetch(`https://api.kreimben.com/get_photos`);
+   json = await res.json();
+ });*/
+
+</script>
+
+<div>
+
+  {#each json as data}
+    <div>
+      {data}
+    </div>
+  {:else}
+    <div class='nothing'>
+      Nothing!
+    </div>
+  {/each}
+
+</div>
+
+
+<style>
+ .nothing {
+   background: red;
+ }
+</style>
---
+++ src/components/Menu.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+
+</script>
+
+<div class="main_menu">
+    <a href='/' class="main_menu_element">
+        Home
+    </a>
+
+    <!--
+         <a href='https://blog.kreimben.com' class='main_menu_element'>
+         Blog
+         </a>
+    -->
+
+    <a href='https://www.instagram.com/kreimben' class='main_menu_element'>
+        Instagram
+    </a>
+
+    <a href='https://github.com/kreimben' class="main_menu_element">
+        Github
+    </a>
+
+    <a href='https://paypal.me/kreimben' class='main_menu_element'>
+        Paypal
+    </a>
+</div>
+
+<style>
+ .main_menu {
+     flex: auto;
+     text-align: center;
+ }
+
+ .main_menu_element {
+     color: green;
+     font-size: 1.4em;
+
+     margin-left: 2%;
+     margin-right: 2%;
+ }
+</style>
