927e9a5de7a9abbf4894482baf5f103bf1635525
Aksidion Kreimben <aksidion@kreimben.com>
2022-03-18 14:25:31 +0900

    Implement basic website.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+ import { onMount } from 'svelte';
+
+ import Menu from '../components/Menu.svelte';
+
+ let json;
+
+ let success;
+
+ onMount(async () => {
+     const res = await fetch(`http://localhost:3000/api/get_photos`);
+     json = await res.json();
+     success = json["success"];
+     console.log('result: ' + JSON.stringify(json));
+
+ });
+</script>
+
+<svelte:head>
+    <title>Kremben.com</title>
+</svelte:head>
+
+<header>
+    <Menu />
+</header>
+
+<main>
+    {#if success === true}
+        <div>
+            {JSON.stringify(json)}
+        </div>
+        <br />
+        <div>
+            {JSON.stringify(json["success"])}
+        </div>
+        <br />
+        <div>
+            {json["success"]}
+        </div>
+    {:else}
+        <div class='nothing'>
+            Nothing to display!
+        </div>
+    {/if}
+
+</main>
+
+
+<style>
+ .nothing {
+     font-size: 3rem;
+     text-align: center;
+     margin-top: 20%;
+ }
+</style>
