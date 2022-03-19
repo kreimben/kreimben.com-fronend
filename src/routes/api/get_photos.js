f26ac7c1db41370c570ed685c2b5dd0d17e9d96b
Aksidion Kreimben <aksidion@kreimben.com>
2022-03-19 14:29:39 +0900

    Write if branches.

--- src/routes/api/get_photos.js
+++ src/routes/api/get_photos.js
@@ -2,6 +2,8 @@ export async function get() {
   const res = await fetch('https://api.kreimben.com/get_photos');
   const json = await res.json();
 
+  if (json["success"] === false) return {body: {"success": false, "message": "Unable to get photos."}}
+
   const list = json["content"];
   let url = [];
 
