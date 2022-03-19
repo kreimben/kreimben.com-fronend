6b078143e9498177a88e8048478ea5044bc7899c
Aksidion Kreimben <aksidion@kreimben.com>
2022-03-19 16:35:58 +0900

    Edit card box.

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -6,16 +6,17 @@
 
 <style>
  .card_image {
-     box-shadow: 0 0 0 0 rgba(0,0,0,0.2);
      transition: 0.2s;
 
-     padding: 1rem;
-     background: red;
-
      margin:0.3em;
+
+     border: 16px solid #fff;
+     border-radius: 16px;
+     background: rgba(255, 255, 255, 0.8);
+     box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
  }
 
  .card_image:hover {
-     box-shadow: 0 8px 16px 0 rgba(0,0,0,1);
+     box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.6);
  }
 </style>
