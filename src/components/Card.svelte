f2de3cb81eac22bbac5542075fe0c8ac94962219
Aksidion Kreimben <aksidion@kreimben.com>
2022-03-19 15:43:09 +0900

    Implement card image views.

---
+++ src/components/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+ export let url;
+</script>
+
+<img alt={url} src={url} class="card_image" />
+
+<style>
+ .card_image {
+     box-shadow: 0 0 0 0 rgba(0,0,0,0.2);
+     transition: 0.2s;
+
+     padding: 1rem;
+     background: red;
+
+     margin:0.3em;
+ }
+
+ .card_image:hover {
+     box-shadow: 0 8px 16px 0 rgba(0,0,0,1);
+ }
+</style>
