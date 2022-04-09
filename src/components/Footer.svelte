54402219b67c57df58e3ed3a0abb5e7a486adf18
Aksidion Kreimben <aksidion@kreimben.com>
2022-04-10 02:57:37 +0900

    Create new component as footer.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,23 @@
+<footer>
+    <div class="footer">
+        <div style="font-size: 1.2rem;">
+            Copyright 2020-2022 Aksidion Kreimben
+        </div>
+        <div style="font-size: 1em;">
+            All rights reserved.
+        </div>
+        <div>
+            <a href="mailto:aksidion@kreimben.com">
+                aksidion@kreimben.com
+            </a>
+        </div>
+    </div>
+</footer>
+
+<style>
+    .footer {
+        background-color: rgba(255, 255, 255, 0.5);
+        text-align: center;
+        margin-bottom: 2em;
+    }
+</style>
